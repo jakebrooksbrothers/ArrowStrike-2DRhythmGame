eeacc7e4ffd89ab83d2487a2c5d1ce99808a6e63
Jacob Sauquillo Brooks <jsbrooks@sdsu.edu>
2020-11-24 00:13:27 -0800

    created script for UI functions for buttons

--- Assets/Scripts/UIcontroller.cs
+++ Assets/Scripts/UIcontroller.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class UIcontroller : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Difficulty()
     {
-        
+
+        SceneManager.LoadScene("DifficultyScene");
     }
 
     // Update is called once per frame
-    void Update()
+    void Play()
+    {
+
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    void Credits()
     {
         
+        SceneManager.LoadScene("CreditScene");
+    }
+
+    void Back()
+    {
+
+        SceneManager.LoadScene("MenuScene");
     }
 }
