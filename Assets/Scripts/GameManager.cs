0082d23a94dc3826ce3ec8c32c7712878921ee64
Jacob Sauquillo Brooks <jsbrooks@sdsu.edu>
2020-11-24 11:04:01 -0800

    added results page and supporting script functions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,12 +22,24 @@ public class GameManager : MonoBehaviour
     public Text scoreText;
     public Text multiText;
 
+    public float totalNotes;
+    public float normalHits;
+    public float goodHits;
+    public float perfectHits;
+    public float missedHits;
+
+    public GameObject resultsScreen;
+    public Text normalsText, goodsText, perfectsText, missesText, finalScoreText;
+
+
     // Start is called before the first frame update
     void Start()
     {
         instance = this;
         scoreText.text = "SCORE: 0";
         currentMulitplier = 1;
+
+        totalNotes = FindObjectsOfType<NoteObject>().Length;
     }
 
     // Update is called once per frame
@@ -41,6 +53,18 @@ public class GameManager : MonoBehaviour
                 theBS.hasStarted = true;
 
                     theMusic.Play();
+            }
+        }
+        else
+        {
+            if(!theMusic.isPlaying && !resultsScreen.activeInHierarchy)
+            {
+                resultsScreen.SetActive(true);
+                normalsText.text = "" + normalHits;
+                goodsText.text = "" + goodHits;
+               perfectsText.text = "" + perfectHits;
+                missesText.text = "" + missedHits;
+                finalScoreText.text = "" + currentScore;
             }
         }
     }
@@ -68,22 +92,26 @@ public class GameManager : MonoBehaviour
         currentMulitplier = 1;
         mulitplierTracker = 0;
         multiText.text = "MULTIPLIER: x" + currentMulitplier;
+        missedHits++;
         
     }
     public void NormalHit()
     {
         currentScore += scorePerNote * currentMulitplier;
         NoteHit();
+        normalHits++;
     }
     public void GoodHit()
     {
         currentScore += scorePerGoodNote * currentMulitplier;
         NoteHit();
+        goodHits++;
     }
     public void PerfectHit()
     {
         currentScore += scorePerPerfectNote * currentMulitplier;
         NoteHit();
+        perfectHits++;
     }
 }
 
